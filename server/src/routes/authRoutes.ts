20785228ae5b834164205bebab369dc605ba3cf9
Prashant Adhikari <student@prashantadhikari7.com.np>
2026-07-26 13:30:33 +0545

    refactor: remove rate limiter middleware and update auth routes

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,9 +1,8 @@
 import { Router } from "express";
 import { logoutHandler, requestOtpHandler, verifyOtpHandler } from "../controllers/authController.js";
-import { otpRequestLimiter } from "../middleware/rateLimiter.js";
 
 export const authRouter = Router();
 
-authRouter.post("/request-otp", otpRequestLimiter, requestOtpHandler);
+authRouter.post("/request-otp", requestOtpHandler);
 authRouter.post("/verify-otp", verifyOtpHandler);
 authRouter.post("/logout", logoutHandler);
